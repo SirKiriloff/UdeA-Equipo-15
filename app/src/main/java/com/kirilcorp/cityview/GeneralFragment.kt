8e714c4f1d8df34e0f9212b306a73c8ca0d5735c
FRANCISCO6547 <75024616+FRANCISCO6547@users.noreply.github.com>
2021-09-26 10:15:22 -0500

    Update GeneralFragment.kt

--- app/src/main/java/com/kirilcorp/cityview/GeneralFragment.kt
+++ app/src/main/java/com/kirilcorp/cityview/GeneralFragment.kt
@@ -46,26 +46,4 @@ class GeneralFragment : Fragment() {
         onNavDestinationSelected(item,requireView().findNavController())
                 || super.onOptionsItemSelected(item)
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @return A new instance of fragment GeneralFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GeneralFragment()
-       /* fun newInstance(param1: String, param2: String) =
-            GeneralFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-
-        */
-    }
 }
