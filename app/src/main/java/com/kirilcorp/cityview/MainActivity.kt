defee033ad0ec6f15a46b098bb43c861bcb9d0d7
FRANCISCO6547 <75024616+FRANCISCO6547@users.noreply.github.com>
2021-09-28 11:20:37 -0500

    Update MainActivity.kt

--- app/src/main/java/com/kirilcorp/cityview/MainActivity.kt
+++ app/src/main/java/com/kirilcorp/cityview/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        //val navController = this.findNavController(R.id.myNavHostFragment)
         return navController.navigateUp()
     }
 }
