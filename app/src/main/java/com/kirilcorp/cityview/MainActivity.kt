e39a17d5ddc64997991f3fb5cf20262749000034
FRANCISCO6547 <75024616+FRANCISCO6547@users.noreply.github.com>
2021-09-28 11:20:24 -0500

    Update MainActivity.kt

--- app/src/main/java/com/kirilcorp/cityview/MainActivity.kt
+++ app/src/main/java/com/kirilcorp/cityview/MainActivity.kt
@@ -3,21 +3,25 @@ package com.kirilcorp.cityview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.kirilcorp.cityview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        val navController = this.findNavController(R.id.myNavHostFragment)
+        navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this,navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.myNavHostFragment)
+        //val navController = this.findNavController(R.id.myNavHostFragment)
         return navController.navigateUp()
     }
 }
