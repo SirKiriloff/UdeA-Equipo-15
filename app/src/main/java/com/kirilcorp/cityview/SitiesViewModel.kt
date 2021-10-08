2e556570a8fdebb2a0d9adba888beb85faa28f3b
FRANCISCO6547 <75024616+FRANCISCO6547@users.noreply.github.com>
2021-10-08 17:43:05 -0500

    Update SitiesViewModel.kt

--- app/src/main/java/com/kirilcorp/cityview/SitiesViewModel.kt
+++ app/src/main/java/com/kirilcorp/cityview/SitiesViewModel.kt
@@ -3,10 +3,34 @@ package com.kirilcorp.cityview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kirilcorp.cityview.data.remote.RetrofitFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SitiesViewModel : ViewModel() {
 
     private val selected = MutableLiveData<Sities>()
+    private var apiService = RetrofitFactory.apiService()
+    private var pois = MutableLiveData<List<Sities>>()
+    var poisLiveData: LiveData<List<Sities>> = pois
+
+    init {
+        getPois()
+    }
+
+    fun getPois() {
+        viewModelScope.launch {
+            pois.value = requestPois()
+        }
+    }
+
+    private suspend fun requestPois(): List<Sities> {
+        return withContext(Dispatchers.IO) {
+            apiService.requestPois()
+        }
+    }
 
     fun getSelected() : LiveData<Sities> = selected
 
