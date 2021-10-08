0dd201c3e45065b7e648dd270d0ac0ba4c4d4646
FRANCISCO6547 <75024616+FRANCISCO6547@users.noreply.github.com>
2021-10-08 16:58:16 -0500

    Update Sities.kt

--- app/src/main/java/com/kirilcorp/cityview/Sities.kt
+++ app/src/main/java/com/kirilcorp/cityview/Sities.kt
@@ -1,8 +1,18 @@
 package com.kirilcorp.cityview
 
+import com.google.gson.annotations.SerializedName
+
 data class Sities (
+
+    @SerializedName("name")
     val name: String,
+
+    @SerializedName("description")
     val descriptio: String,
+
+    @SerializedName("score")
     val score: String,
+
+    @SerializedName("image")
     var image: String
 )
