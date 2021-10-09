aa3762ebc86d826c28a782d78bf514fc73f02dac
FRANCISCO6547 <75024616+FRANCISCO6547@users.noreply.github.com>
2021-10-09 18:14:27 -0500

    Update DetailFragment.kt

--- app/src/main/java/com/kirilcorp/cityview/DetailFragment.kt
+++ app/src/main/java/com/kirilcorp/cityview/DetailFragment.kt
@@ -1,9 +1,12 @@
 package com.kirilcorp.cityview
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
@@ -17,7 +20,9 @@ class DetailFragment : Fragment() {
     private lateinit var model: SitiesViewModel
     private lateinit var titleView: TextView
     private lateinit var descriptionView: TextView
+    private lateinit var temperatureView: TextView
     private lateinit var imgView: ImageView
+    private lateinit var ubicationBtn: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +32,12 @@ class DetailFragment : Fragment() {
             R.layout.fragment_detail,container,false)
         descriptionView=binding.infoGeneral
         titleView=binding.titleDetail
+        temperatureView=binding.temperatureDetail
         imgView=binding.placeImg
+        ubicationBtn=binding.btnUbication
+        ubicationBtn.setOnClickListener {
+            ubicationClick(ubicationBtn.text as String)
+        }
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -41,7 +51,16 @@ class DetailFragment : Fragment() {
         model.getSelected().observe(viewLifecycleOwner, { site ->
             titleView.text = site.name
             descriptionView.text = site.descriptio
+            temperatureView.text = site.temperature
+            ubicationBtn.text = site.ubication
             Picasso.get().load(site.image).into(imgView)
         })
+    }
+
+    private fun ubicationClick(ubi: String){
+        val gmmIntentUri = Uri.parse("geo:$ubi")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        startActivity(mapIntent)
     }
 }
